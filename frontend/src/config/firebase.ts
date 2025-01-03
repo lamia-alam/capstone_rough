516ea8371da4eebdbcf7cbb8b8bea65da879b99a
lamia-alam <lamia.alam@outlook.com>
2025-01-02 22:28:05 -0500

    added .env file

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -6,12 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA8XV-zjLAFa91fR29vSHcA5vpMEWCBp_k",
-  authDomain: "capstone-9ffce.firebaseapp.com",
-  projectId: "capstone-9ffce",
-  storageBucket: "capstone-9ffce.firebasestorage.app",
-  messagingSenderId: "814228452307",
-  appId: "1:814228452307:web:251ee2ae50fd6e404f7526"
+  
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID ,
+  appId: import.meta.env.VITE_FB_APP_ID
 };
 
 // Initialize Firebase
